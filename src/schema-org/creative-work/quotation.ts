93f7587f4d67edf9cd81784fb9cd45c935198628
Jeff Eaton <jeff@eaton.fyi>
2024-04-03 23:54:46 -0500

    Notes on quotations

--- src/schema-org/creative-work/quotation.ts
+++ src/schema-org/creative-work/quotation.ts
@@ -4,6 +4,12 @@ import { CreativeWorkSchema } from './creative-work.js';
 import { PersonSchema } from '../person.js';
 
 // See https://schema.org/Quotation for details.
+// thing.partOf should be used to indicate the creative work the Quotation
+// is sourced from. creativeWork.creator should be used to indicate an individual
+// speaker; if multiples exist it's permissible to refer ONLY to the work the
+// quote appears in.
+// If the speaker is a fictional character, quotation.spokenBy should store a
+// reference to that speaker's person record, or a name/name+url/name+etc bundle.
 export const QuotationSchema = CreativeWorkSchema.extend({
   type: z.literal('quotation').default('quotation'),
   location: z.string().optional().describe("A duration, page number, or other indication of where the quote can be found inf the work it belongs to."),
