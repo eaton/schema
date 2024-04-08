ff70de4708ebf795ece596adfa244db6a67abb05
Jeff Eaton <jeff@eaton.fyi>
2024-04-08 16:56:16 -0500

    No need to store full mimetype here

--- src/internal/body.ts
+++ src/internal/body.ts
@@ -2,22 +2,22 @@ import { z } from 'zod';
 
 // Supported MimeTypes will probably be something along the lines of:
 //
-// - text/plain
-// - text/markdown
-// - text/html
-// - text/csv
-// - text/tsv
-// - application/xml
-// - application/json
-// - application/ld+json
-// - application/x-ndjson 
+// - plain
+// - markdown
+// - html
+// - csv
+// - tsv
+// - xml
+// - json
+// - ld+json
+// - x-ndjson 
 
 // @see {@link https://www.iana.org/assignments/markdown-variants/markdown-variants.xhtml}
 
 export const BodySchema = z.object({
   entity: z.string(),
   property: z.string().default('body'),
-  mime: z.string().default('text/plain'),
+  mime: z.string().default('plain'),
   variation: z.string().optional().describe('Stores variation names for meaningfully different versions of the same content. (Different reading levels, etc).'),
   value: z.string()
 })
